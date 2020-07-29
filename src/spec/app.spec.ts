cda367cc8e782f808ff799473c4799b241d6a146
Emma <eerandall1@gmail.com>
2020-07-29 17:00:45 +0100

    next app spec in api

--- src/spec/app.spec.ts
+++ src/spec/app.spec.ts
@@ -2,20 +2,20 @@ import { expect } from 'chai';
 import supertest from 'supertest';
 import app from '../app';
 
-process.env.NODE_ENV = 'test';
-
 const request = supertest(app);
 
-describe('/', () => {
-  it('GET status 200 responds with JSON describing all available endpoints in API', () => {
-    return request
-      .get('/')
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.endpoints).to.be.an('object');
-        expect(body.endpoints['/']).to.equal(
-          'serving a list of endpoints and available methods',
-        );
-      });
+describe('API', () => {
+  describe('/', () => {
+    it('GET status 200 responds with JSON describing all available endpoints in API', () => {
+      return request
+        .get('/')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.endpoints).to.be.an('object');
+          expect(body.endpoints['/']).to.equal(
+            'serving a list of endpoints and available methods',
+          );
+        });
+    });
   });
 });
