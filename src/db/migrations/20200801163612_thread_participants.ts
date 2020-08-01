1d164aa10eacda7ca8be3517c4219c7e816026cd
Emma <eerandall1@gmail.com>
2020-08-01 16:52:22 +0100

    thread participants junction table

---
+++ src/db/migrations/20200801163612_thread_participants.ts
@@ -0,0 +1,18 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('thread_participants', (table) => {
+    // id
+    table.integer('thread_id').notNullable();
+    table.integer('profile_id').notNullable();
+
+    table.foreign('thread_id').references('id').inTable('threads');
+    table.foreign('profile_id').references('id').inTable('profiles');
+    // no duplicate participants in threads
+    table.unique(['thread_id', 'profile_id']);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('thread_participants');
+}
