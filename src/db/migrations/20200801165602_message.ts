acbda2718328c4c93b1ff6e9d928caaef76a9153
Emma <eerandall1@gmail.com>
2020-08-01 18:57:11 +0100

    message knex schema

---
+++ src/db/migrations/20200801165602_message.ts
@@ -0,0 +1,20 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('messages', (table) => {
+    table.increments();
+    table.integer('thread_id').notNullable();
+    table.integer('profile_id').notNullable();
+    table.text('text');
+    // for emoji code, profile_id and emoji reaction count
+    table.json('reactions');
+    table.timestamps(true, true);
+
+    table.foreign('thread_id').references('id').inTable('threads');
+    table.foreign('profile_id').references('id').inTable('profiles');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('messages');
+}
