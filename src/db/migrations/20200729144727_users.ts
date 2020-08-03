8ea9dfae8becb27fa2bd451d9aa1a4b8078faf7b
Emma <eerandall1@gmail.com>
2020-08-03 14:36:24 +0100

    add password field to user table in migration

--- src/db/migrations/20200729144727_users.ts
+++ src/db/migrations/20200729144727_users.ts
@@ -7,6 +7,7 @@ export async function up(knex: Knex): Promise<void> {
     table.string('email').unique().notNullable();
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
+    table.string('password').notNullable();
     table.timestamps(true, true);
   });
 }
