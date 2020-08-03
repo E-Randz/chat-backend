c04ebea3a21d6f1551e7467254923a6e76c2c4ce
Emma <eerandall1@gmail.com>
2020-08-03 17:37:14 +0100

    seed data users teams and profiles

--- src/db/seed-data/index.ts
+++ src/db/seed-data/index.ts
@@ -5,5 +5,15 @@ const env = process.env.NODE_ENV;
 // depending on the env select the correct json data file to parse
 
 // USERS
-const rawUserData = fs.readFileSync(`./${env}-data/users.json`);
+const rawUserData = fs.readFileSync(__dirname + `/${env}-data/users.json`);
 export const users = JSON.parse(rawUserData.toString());
+
+// TEAMS
+const rawTeamData = fs.readFileSync(__dirname + `/${env}-data/teams.json`);
+export const teams = JSON.parse(rawTeamData.toString());
+
+// PROFILES
+const rawProfileData = fs.readFileSync(
+  __dirname + `/${env}-data/profiles.json`,
+);
+export const profiles = JSON.parse(rawProfileData.toString());
--- src/db/seeds/03_profiles.ts
+++ src/db/seeds/03_profiles.ts
@@ -1,10 +1,10 @@
 import * as Knex from 'knex';
-import { teams } from '../seed-data/index';
+import { profiles } from '../seed-data/index';
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
-  await knex('teams').del();
+  await knex('profiles').del();
 
   // Inserts seed entries
-  await knex('teams').insert(teams);
+  await knex('profiles').insert(profiles);
 }
