489b0305b06d9106be5072318c82fedba36f7bda
Emma <eerandall1@gmail.com>
2020-08-07 10:57:57 +0100

    create a ts config file that changes with node env

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
 import knex from 'knex';
 const knexfile = require('../knexfile');
+import config from '../config';
 
-const env = process.env.NODE_ENV || 'development';
+const env = config.ENV || 'dev';
 const configOptions = knexfile[env];
 
 export default knex(configOptions);
